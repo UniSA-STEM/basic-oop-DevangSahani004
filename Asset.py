9cd99ffdfd1a777a0ad4eaddb3deaff8041d7709
Devang Sahani <133926800+DevangSahani004@users.noreply.github.com>
2025-10-23 18:28:24 +1030

    Comments added to ensure understanding of code along with docstring to serve definition of the module.

--- Asset.py
+++ Asset.py
@@ -7,26 +7,32 @@ Username: sahdy004
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 class Asset:
+    """Represents a digital asset with a name, description, and encryption status."""
+    # constructor initialises asset details through private attributes
     def __init__(self, name, description):
         self.__name = name
         self.__description = description
         self.__encrypted = False
 
+    # use of getters to retrieve name and asset description for private visibility
     def get_name(self):
         return self.__name
 
     def get_asset_description(self):
         return self.__description
 
+    # ensures encryption of assets in hacker.py
     def is_encrypted(self):
         return self.__encrypted
 
+    # used as setter to set status of assets with encryption and decryption
     def encrypt_asset(self):
         self.__encrypted = True
 
     def decrypt_asset(self):
         self.__encrypted = False
 
+    # string conversion method to display encryption or not on output to user
     def __str__(self):
         if self.__encrypted:
             return f"{self.__name}: {self.__description} [Encrypted]"
